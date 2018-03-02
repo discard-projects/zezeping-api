07292a51e002975db08e6c8c58ba7ed1c6791670
mars <578595193@qq.com>
2018-03-02 21:44:47 +0800

    FEAT: region

--- app/controllers/admin/regions_controller.rb
+++ app/controllers/admin/regions_controller.rb
@@ -1,2 +1,6 @@
 class Admin::RegionsController < ApplicationController
+  def index
+    regions = Region.roots
+    super regions
+  end
 end
--- app/forms/region_form/create.rb
+++ app/forms/region_form/create.rb
@@ -3,6 +3,7 @@ module RegionForm
     model :region
 
     property :name
+    property :parent_id
 
     validates :name, presence: true, length: { maximum: 40 }
   end
