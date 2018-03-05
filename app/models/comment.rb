f1e0bd44c866682b5db3a247971cf36ef19e364c
mars <578595193@qq.com>
2018-03-05 21:26:22 +0800

    create comment update store

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,19 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :store, counter_cache: true
+
+  after_commit :update_store, on: :create
+
+  private
+
+  def update_store
+    sum_rank_taste, sum_rank_env, sum_rank_service = store.comments.pluck('sum(rank_taste)', 'sum(rank_env)', 'sum(rank_service)')[0]
+    total_count = store.comments.count
+    store.update(rank: store.comments.sum(:rank)/total_count)
+    store.store_detail.update({
+      rank_taste: sum_rank_taste / total_count,
+      rank_env: sum_rank_env / total_count,
+      rank_service: sum_rank_service / total_count
+    })
+  end
 end
