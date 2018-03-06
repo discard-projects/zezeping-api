dade2172ab36da22fab904d97d72f9a8746cf828
mars <578595193@qq.com>
2018-03-06 14:32:55 +0800

    add comment

--- app/forms/comment_form/create.rb
+++ app/forms/comment_form/create.rb
@@ -4,9 +4,11 @@ module CommentForm
 
     property :store_id
     property :user_id
+    property :rank
     property :content
 
     validates :store_id, presence: true
+    validates :rank, presence: true, numericality: { greater_than: 0 }
     validates :content, presence: true, length: { maximum: 255 }
   end
 end
