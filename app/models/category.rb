c4bde8e4e849e8999c67b1ddf2069e8b8c7d988b
mars <578595193@qq.com>
2018-03-08 19:01:19 +0800

    FIX

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,9 +3,10 @@ class Category < ApplicationRecord
 
   has_many :stores
 
+  has_many :subtree_stores, through:
+
   # subtree 包含自身和所有子孙
   def subtree_stores
-    sql = subtree.joins(:stores).select('stores.*').to_sql
-    Store.find_by_sql(sql)
+    Store.joins(:category).merge(subtree)
   end
 end
