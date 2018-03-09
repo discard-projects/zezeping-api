8967de930fd801d5cac5efea963aabe3e7e73729
mars <578595193@qq.com>
2018-03-09 19:39:47 +0800

    oauth only create write nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,16 @@ class User < ApplicationRecord
           :confirmable
   include DeviseTokenAuth::Concerns::User
 
+  before_update :check_nickname
+
   def sec_email
     email.gsub(/(\A.{2})(.*)(@.*\z)/){"#{$1}***#{$3}"}
+  end
+
+  private
+
+  # 授权登录操作 不更新nickname # sign_in_count 和 nickname 同时存在认为 是 oath认证登录操作
+  def check_nickname
+    self.restore_nickname! if changes['nickname'].present? && changes['sign_in_count'].present?
   end
 end
