663ae9089d8b694e4aa20e12e1f144f56be4bba7
mars <578595193@qq.com>
2018-03-15 14:18:38 +0800

    FIX

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  belongs_to :store, dependent: :destroy
+  belongs_to :store
   belongs_to :category, optional: true
   has_many :comments, :as => :commentable
   has_many :attachment_images, as: :owner, dependent: :destroy
