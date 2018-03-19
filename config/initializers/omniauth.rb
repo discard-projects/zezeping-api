92377e93f9b401634be518b33ba5679b7426acd2
mars <578595193@qq.com>
2018-03-20 00:30:33 +0800

    FIX

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :wechat, $env['OMNIAUTH']['WECHAT']['APP_ID'], $env['OMNIAUTH']['WECHAT']['APP_SECRET']
-  provider :facebook, $env['omniauth']['facebook']['key'], $env['omniauth']['facebook']['secret'], scope: 'public_profile,email'
+  provider :facebook, $env['omniauth']['facebook']['key'], $env['omniauth']['facebook']['secret'], scope: 'public_profile,email', callback_url: $env['omniauth']['facebook']['callback_url']
 end
