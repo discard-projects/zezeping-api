f586ad4051ff484dd7cd1d969430a1f53468558b
mars <578595193@qq.com>
2018-03-20 09:14:37 +0800

    fix

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,3 +1,9 @@
 class Api::V1::StoresController < Api::V1::BaseController
   skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    super do |stores|
+      stores.order(suggested_at: :desc)
+    end
+  end
 end
