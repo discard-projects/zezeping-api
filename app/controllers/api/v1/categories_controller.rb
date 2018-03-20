b5c23a58a89aecfcae52a1a787fb9a90622e4e83
mars <578595193@qq.com>
2018-03-21 00:22:13 +0800

    distance

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::CategoriesController < Api::V1::BaseController
   skip_before_action :authenticate_user!, only: [:show, :stores]
   def stores
-    @stores = br_index Category.find_by(id: params[:id]).subtree_stores.order(comments_count: :desc, updated_at: :desc)
+    stores =  Category.find_by(id: params[:id]).subtree_stores
+    case params[:sort_type]
+      when 'popularity'
+        stores = stores.order(comments_count: :desc, updated_at: :desc)
+      when 'distance'
+        stores = stores.near([loc['lat'], loc['lng']], 100).order(comments_count: :desc, updated_at: :desc)
+      else # intelligent（智能）
+        stores = stores.order(suggested_at: :desc, comments_count: :desc, updated_at: :desc)
+    end
+    br_index stores
     render 'api/v1/stores/index'
+  end
+
+  private
+
+  def loc
+    JSON.parse params[:loc]
   end
 end
--- app/views/api/v1/stores/index.json.jbuilder
+++ app/views/api/v1/stores/index.json.jbuilder
@@ -1,6 +1,7 @@
 json.partial! 'partial/paginate_meta', object: @stores
 json.items @stores do |store|
   json.(store, :id, :name, :rank, :comments_count, :per_expense)
+  json.distance store.try(:distance)
   json.logo store.logo.thumb2
   json.region_name store.region.try(:name)
   json.category_names store.categories.pluck(:name)
