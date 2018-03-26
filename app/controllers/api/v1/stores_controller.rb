3f13eb4e983e5f44a3f25040ecc0d2a01255945f
mars <578595193@qq.com>
2018-03-26 10:04:41 +0800

    FIX

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::StoresController < Api::V1::BaseController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :detail]
 
   def index
     super do |stores|
